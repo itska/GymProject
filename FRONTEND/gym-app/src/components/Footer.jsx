a7df4ee194947a628b4c04f4700ca3eb9fe24675
itska <ka574018@gmail.com>
2024-12-10 11:43:36 +0530

    Initial commit for GymProject

---
+++ FRONTEND/gym-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Kumar Aryan
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ FRONTEND/gym-app/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='content'>
+        <div className='title'>
+          <h1>Sweat</h1>
+          <h1>It</h1>
+          <h1>Out!</h1>
+        </div>
+        <div className='sub-title'>
+          <p>Your Journey to Fitness starts here.</p>
+          <p>Unleash Your Potential</p>
+        </div>
+        <div className='buttons'>
+          <button>Start Your Journey</button>
+          <button>Browse Plans</button>
+        </div>
+      </div>
+      </section>
+  )
+}
+
+export default Hero
